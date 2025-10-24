7183c483314917289fabc849649566605207747c
Hennadii.Khodyriev@mit.khpi.edu.ua <Hennadii.Khodyriev@mit.khpi.edu.ua>
2025-10-24 09:32:26 +0300

    JS basics variables

---
+++ script.js
@@ -0,0 +1,51 @@
+// console.log("I'm here!");
+// alert('hi'); 
+// alert('world');
+
+/*
+alert(3 +
+    1
+    + 2);
+*/
+
+// ключові слова для оголошення змінних
+/*
+let - значення змінної можна перевизначати
+const - константа - не перевизначається
+var - застаріла практика
+*/
+
+let message;
+message = 'Hi there';
+message = 233;
+
+// let age = 23;
+
+const birthday = '24.09.2000';
+// birthday = 444; - викличе помилку
+
+
+// let user = 'Іван';
+// let age = 25;
+// let message = 'Привіт';
+
+// let user = 'Іван', age = 25, message = 'Привіт';
+
+// let user, age, message;
+// user = 'Іван';
+// age = 25;
+// message = 'Привіт';
+
+// блоком коду у JS є будь який шматочок загорнутий у { }
+
+// console.log('user:', user)
+// console.log(age)
+// console.log(message)
+
+let userName;
+let test123;
+let $ = 1; // оголошено змінну з ім’ям "$"
+let _ = 2; // а тепер змінна з ім’ям "_"
+
+// let 123 - викликає помилку! Імʼя змінної не може починатися з цифри
+
